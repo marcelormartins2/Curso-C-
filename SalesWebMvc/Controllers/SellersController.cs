a23e3355d3df294ea6907e4f7906229f5a17d801
marcelormartins2 <marcelormartins2@gmail.com>
2020-04-27 13:30:16 -0300

    Controler, view e service de Seller sem scafold

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Data;
+using SalesWebMvc.Services;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        public readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_sellerService.FindAll());
+        }
+    }
+}
