6334a25756c490404b4ce600efffcd6964a130d6
marcelormartins2 <marcelormartins2@gmail.com>
2020-04-27 01:16:34 -0300

    Departament Control com scafolded

--- SalesWebMvc/Models/Departament.cs
+++ SalesWebMvc/Models/Departament.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SalesWebMvc.Controllers
+namespace SalesWebMvc.Models
 {
     public class Departament
     {
