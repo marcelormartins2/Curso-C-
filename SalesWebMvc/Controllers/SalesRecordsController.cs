f75bd2c947cc186442e6bfd3f37c0eb9c4b2438b
marcelormartins2 <marcelormartins2@gmail.com>
2020-04-27 17:28:00 -0300

    Scafold salesrecod

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using SalesWebMvc.Services;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        private readonly SalesRecordsService _salesRecordsService;
+
+        public SalesRecordsController(SalesRecordsService salesRecordsService)
+        {
+            _salesRecordsService = salesRecordsService;
+        }
+
+        // GET: SalesRecords
+        public async Task<IActionResult> Index()
+        {
+            return View(await _salesRecordsService.FindAllAsync());
+        }
+
+        // GET: SalesRecords/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salesRecord = await _salesRecordsService.FindAsync(id.Value);
+            if (salesRecord == null)
+            {
+                return NotFound();
+            }
+
+            return View(salesRecord);
+        }
+
+        // GET: SalesRecords/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SalesRecords/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Date,Amount,Status")] SalesRecord salesRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                await _salesRecordsService.InsertAsync(salesRecord);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salesRecord);
+        }
+
+        // GET: SalesRecords/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salesRecord = await _salesRecordsService.FindAsync(id.Value);
+            if (salesRecord == null)
+            {
+                return NotFound();
+            }
+
+            return View(salesRecord);
+        }
+
+        // POST: SalesRecords/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,Amount,Status")] SalesRecord salesRecord)
+        {
+            if (id != salesRecord.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _salesRecordsService.UpdatetAsync(salesRecord);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                        return NotFound();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salesRecord);
+        }
+
+        // GET: SalesRecords/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salesRecord = await _salesRecordsService.FindAsync(id.Value);
+            if (salesRecord == null)
+            {
+                return NotFound();
+            }
+
+            return View(salesRecord);
+        }
+
+        // POST: SalesRecords/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _salesRecordsService.RemoveAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ SalesWebMvc/Services/SalesRecordsService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using SalesWebMvc.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordsService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SalesRecordsService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<SalesRecord> FindAsync(int id) {
+            return await _context.SalesRecord.FirstOrDefaultAsync(obj => obj.Id == id);
+        }
+        public async Task<List<SalesRecord>> FindAllAsync() {
+            return await _context.SalesRecord.ToListAsync();
+        }
+
+        public async Task InsertAsync(SalesRecord obj)
+        {
+            _context.Add(obj);
+            await _context.SaveChangesAsync();
+        }
+     
+        public async Task UpdatetAsync(SalesRecord obj)
+        {
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
+            if (!hasAny)
+            {
+                //throw new NotFoundException("Id not found");
+            }
+            else
+            {
+                //try
+                //{
+                    _context.Update(obj);
+                    await _context.SaveChangesAsync();
+                //}
+                //catch (DbUpdateConcurrencyException e)
+                //{
+                //    throw new DbConcurrencyException(e.Message);
+                //}
+            }
+        }
+
+        internal async Task RemoveAsync(int id)
+        {
+            var obj = await _context.SalesRecord.FindAsync(id);
+            _context.SalesRecord.Remove(obj);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
